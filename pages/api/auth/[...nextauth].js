94bb3822f20b4978573dea29c7992f3f0b239f9f
suraj105 <63006660+suraj105@users.noreply.github.com>
2022-01-03 23:43:32 +0100

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,5 @@
-import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
@@ -10,4 +8,3 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
-});
