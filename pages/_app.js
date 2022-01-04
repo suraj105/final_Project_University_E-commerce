a41574e9fa3a09091e4cc8d5d0a6388a2fbdaf41
suraj105 <63006660+suraj105@users.noreply.github.com>
2022-01-04 02:56:09 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import { SessionProvider } from "next-auth/react";
 import { store } from "../app/store";
 import { Provider } from "react-redux";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={pageProps.session}>
       <Provider store={store}>
